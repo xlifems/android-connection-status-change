17c359584446956c6b22f76da66c05b7b40c2045
FelixAdrian\Felix Adrian <felix.masa@servinformacion.com>
2018-11-11 12:50:15 -0500

    Primera subida

---
+++ app/src/main/java/xlifems/com/monitorconntionstatuschange/MainActivity.java
@@ -0,0 +1,13 @@
+package xlifems.com.monitorconntionstatuschange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
